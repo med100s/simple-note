eccd03b9dcf7c1c8ecd90b403194ac1abbd6f73a
med100s <alphawarrior999@gmail.com>
2022-08-21 02:16:51 +0500

    added simple logo

--- src/backgroundCanvas.js
+++ src/backgroundCanvas.js
@@ -19,8 +19,8 @@ export default function BackgroundCanvas() {
 
         let ctx = canvas.getContext('2d');
 
-
-
+        
+        
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = window.innerHeight;
 
